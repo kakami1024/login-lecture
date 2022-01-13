23ce0890a892ec0e4b9223c0a25440a463052bec
kakami1024 <kakami1024@gmail.com>
2022-01-14 06:02:43 +0900

    0114(회원가입 요청)

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -3,10 +3,11 @@
 const express = require('express');
 const router = express.Router(); // router 사용
 
-const ctrl = require("./home.ctrl") // controller 가져오기
+const ctrl = require('./home.ctrl'); // controller 가져오기
 
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
 router.post('/login', ctrl.process.login);
 
 // 외부파일에서 사용할 수 있도록 해준다.
