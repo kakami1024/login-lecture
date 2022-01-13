0be899285f969bfa54bf9c3b6adf0ce22579cce1
kakami1024 <kakami1024@gmail.com>
2022-01-14 07:57:10 +0900

    0114(fs모듈을 통한 로그인 기능 구현)

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,22 @@
 'use strict';
 
+const fs = require('fs').promises;
+
 class UserStorage {
-  static #users = {
-    // #users -> 은닉화
-    id: ['wjs', '김코딩', '나개발'],
-    psword: ['1234', '1234', '123456'],
-    name: ['node', 'java', '서버'],
-  };
+  static #getUserInfo(data, id) { // 프라이빗한 변수나 메서드는   최상단으로 위치해야한다.
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // -> [id,psword,name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
+  }
 
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -20,19 +27,16 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const userKeys = Object.keys(users); // -> [id,psword,name]
-    const userInfo = userKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-
-    return userInfo;
+    return fs
+      .readFile('./src/databases/users.json')
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -16,12 +16,12 @@ const output = {
 };
 
 const process = {
-  login: (req, res) => {
+  login: async (req, res) => {
     const user = new User(req.body);
-    const response = user.login();
+    const response = await user.login();
     return res.json(response);
   },
-  register: (req, res) => {
+  register: (req, res) => { 
     const user = new User(req.body);
     const response = user.register();
     return res.json(response);
