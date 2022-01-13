9af53f3a5661ae400679d24254a414df500a397a
kakami1024 <kakami1024@gmail.com>
2022-01-13 21:43:08 +0900

    0113(DOM을 이용)

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 'use strict';
 
-console.log('hello');
-console.log('bye');
+const id = document.querySelector('#id'),
+  psword = document.querySelector('#psword'),
+  loginBtn = document.querySelector('button'); // button tag는 하나여서 button을 가져올 수 있다.
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+}
